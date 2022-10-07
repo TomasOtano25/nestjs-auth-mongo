fabdc65ddcbf45b4612cae042a9a6dc7499721aa
Tomas Garcia <tomasotano25@gmail.com>
2022-10-07 13:01:00 -0400

    added controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsController } from './controllers/products/products.controller';
-import { CategoriesController } from './controllers/categories/categories.controller';
+import { ProductsController } from './products/products.controller';
+import { CategoriesController } from './categories/categories.controller';
+import { OrdersController } from './orders/orders.controller';
+import { UsersController } from './users/users.controller';
+import { CustomersController } from './customers/customers.controller';
+import { BrandsController } from './brands/brands.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
+  controllers: [
+    AppController,
+    ProductsController,
+    CategoriesController,
+    OrdersController,
+    UsersController,
+    CustomersController,
+    BrandsController,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
