42cbfcb9498490e9d8b8e86002d5189807fe5ade
Tomas Garcia <tomasotano25@gmail.com>
2022-10-14 12:03:33 -0400

    clase 12 validacion de .envs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
-
 import { enviroments } from './eviroments';
 import config from './config';
 
@@ -18,6 +17,11 @@ import config from './config';
       envFilePath: enviroments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     HttpModule,
     ProductsModule,
