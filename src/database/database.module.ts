5765fda45efb5fcb397b189d2d329c542e9cf8da
Tomas Garcia <tomasotano25@gmail.com>
2022-10-20 20:11:50 -0400

    clase 09 usando variables de entorno en mongo

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,6 +1,8 @@
 import { Module, Global } from '@nestjs/common';
-
+import { ConfigType } from '@nestjs/config';
 import { MongoClient } from 'mongodb';
+
+import config from '../config';
 
 // async function run() {
 //   await client.connect();
@@ -15,13 +17,17 @@ import { MongoClient } from 'mongodb';
   providers: [
     {
       provide: 'MONGO',
-      useFactory: async () => {
-        const url = 'mongodb://root:root@localhost:27017';
+      useFactory: async (configService: ConfigType<typeof config>) => {
+        const { connection, user, password, host, port, dbName } =
+          configService.mongo;
+
+        const url = `${connection}://${user}:${password}@${host}:${port}`;
         const client = new MongoClient(url);
         await client.connect();
-        const database = client.db('platzi-store');
+        const database = client.db(dbName);
         return database;
       },
+      inject: [config.KEY],
     },
   ],
   exports: ['MONGO'],
